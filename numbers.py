0eb8c7d9912e7bcdbab037fa5b1fb74bf373b84f
Nikheel Soni <nikheel@amazon.com>
2021-12-28 20:23:04 +0200

    numbers

---
+++ numbers.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Dec 28 19:57:13 2021
+
+@author: nikheel
+"""
+
+print (type(5))
+print (type(3.142))
+print (5/5) # when you use division, you will always get back a float
+print (5 // 5) # integer division
+print ( 5 ** 5 ) # power
+print (10 % 3) # will return the remainder
+
+# BIDMAS governs order of operations
+
+age = 25
+print (age)
+print (age + 5)
+age = age + 5
+print (age)
+age += 5 # short hand notation
+print (age)
+
+wages = 1000
+bills = 200
+rent = 500
+food = 200
+
