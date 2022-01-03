f86512e0a6e1181c9ed580169418c7c40a1eef89
Nikheel Soni <nikheel@amazon.com>
2022-01-03 20:18:09 +0200

    strings

---
+++ 04 strings.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jan  3 20:13:03 2022
+
+@author: nikheel
+"""
+
+string = "he's a mad man"
+string = 'he\'s a mad man'
+
+# for count, value in enumerate(string):
+    # print (count, value)
+    
+greet = 'hello'
+    
+print (greet[0])
